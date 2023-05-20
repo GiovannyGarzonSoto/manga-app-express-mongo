c42dc716a47395104083775af2091644cccb9671
GiovannyGarzon <giovankyandres@gmail.com>
2023-05-20 08:07:50 -0400

    mongodb atlas database added

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import {connect} from 'mongoose'
 
 export const connectDatabase = async() => {
     try{
-        await connect(process.env.DATABASE)
+        await connect(`mongodb+srv://${process.env.ATLAS_URI}/manga-app?retryWrites=true&w=majority`)
         console.log('Database is connected')
     }catch(err){
         console.log(err)
